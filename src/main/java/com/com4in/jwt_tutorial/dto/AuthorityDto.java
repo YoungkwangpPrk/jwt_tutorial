969bc26e06cde1ffd3ed6f0182687bfcf5b10c47
ykPark <pyk2191@nate.com>
2023-02-10 18:41:35 +0900

    생성230210

---
+++ src/main/java/com/com4in/jwt_tutorial/dto/AuthorityDto.java
@@ -0,0 +1,12 @@
+package com.com4in.jwt_tutorial.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthorityDto {
+    private String authorityName;
+}
---
+++ src/main/java/com/com4in/jwt_tutorial/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.com4in.jwt_tutorial.repository;
+
+import com.com4in.jwt_tutorial.entity.Users;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<Users, Long> {
+    @EntityGraph(attributePaths = "authorities")
+    Optional<Users> findOneWithAuthoritiesByUsername(String username);
+}
